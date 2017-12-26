e00668dcb30202845e42b56aef865ff2e2e3f432
Liaohuanle <18770913652@163.com>
2017-12-26 14:28:55 +0800

    save code

--- route/index.js
+++ route/index.js
@@ -1,6 +1,6 @@
-const countDown = require('../util/index')
+const { winners, videoid, setIntervalTimeNode, aboutTagList  } = require('../util/index')
 module.exports = (app) => {
   app.get('/', function(req, res) {
-    res.render('../views/layout', { countDown })
+    res.render('../views/layout', { winners, videoid, setIntervalTimeNode, aboutTagList })
   })
 }
