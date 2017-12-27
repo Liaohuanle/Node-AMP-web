fbc9a0be1314b7bb3db0300dde557cd9a7554341
Liaohuanle <18770913652@163.com>
2017-12-27 18:19:09 +0800

    save

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const routes = require('./route')
 const app = new express()
 app.use('/assets', express.static(__dirname + '/assets'))
+
 app.set('view engine', 'ejs')
 routes(app)
 app.listen(3000, (err) => {
