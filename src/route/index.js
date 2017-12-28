ca4796085e2e03fcd12de71d80f70b610b5a4d3d
Liaohuanle <18770913652@163.com>
2017-12-28 18:48:45 +0800

    save code

--- src/route/index.js
+++ src/route/index.js
@@ -1,6 +1,6 @@
 const { winners, winnerNameList, meetUserList, videoid, setIntervalTimeNode, aboutTagList  } = require('../util/index')
 module.exports = (app) => {
-  app.get('/', function(req, res) {
+  app.get('/act/India', function(req, res) {
     res.render('../views/layout', { winners, videoid, setIntervalTimeNode, aboutTagList, winnerNameList, meetUserList })
   })
 }
