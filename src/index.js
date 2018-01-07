35e4b6ff079fd59ce386831377b91270680d0a91
Liaohuanle <18770913652@163.com>
2018-01-07 16:56:00 +0800

    save code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = require('./route')
+const compress = require('./compress')
 const app = new express()
 app.use( '/act/assets/', express.static(__dirname + '/assets/'))
 
 app.set('view engine', 'ejs')
+// compress()
 routes(app)
 app.listen(3000, (err) => {
   console.info(err || 'success')
