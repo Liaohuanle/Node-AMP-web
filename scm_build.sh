20117bd3d1de483f6f4430d92e529cf644cf1003
tea.liao <tea.liao@bytedance.com>
2018-03-30 11:21:19 +0800

    optimise

---
+++ scm_build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#!/bin/bash
+
+DIR=`pwd`
+OUTPUT="$DIR/output"
+RESOURCE="$DIR/output_resource"
+
+rm -rf $OUTPUT
+rm -rf $RESOURCE
+
+
+if [ -f '/etc/profile' ]
+then
+    source /etc/profile
+fi
+
+if [ -d './node_modules' ]
+then
+    echo "node_modules exist"
+else
+    mkdir node_modules
+fi
+
+# 使用node8构建
+nvm use stable
+ 
+./build.sh
+./play.sh
+
+
+mv output output_resource
