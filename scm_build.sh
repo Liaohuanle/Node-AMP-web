08d7f9f87b6c1247602d8ef95947dd0ddc2cfad2
tea.liao <tea.liao@bytedance.com>
2018-03-30 16:52:41 +0800

    chang scm build sh file

--- scm_build.sh
+++ scm_build.sh
@@ -8,12 +8,12 @@ RESOURCE="$DIR/output_resource"
 rm -rf $OUTPUT
 rm -rf $RESOURCE
 
-mkdir OUTPUT
-mkdir RESOURCE
+mkdir $OUTPUT
+mkdir $RESOURCE
 
 if [ -f '/etc/profile' ]
 then
-    source /etc/profile
+  source /etc/profile
 fi
 
 # 使用node8构建
@@ -22,5 +22,5 @@ nvm use stable
 ./build.sh
 
 mv output output_resource
-cd output
+cd $OUTPUT
 node index.js
