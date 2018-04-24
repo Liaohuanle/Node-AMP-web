275409936c077c4600b5635def6f96ba25a1f910
tea.liao <tea.liao@bytedance.com>
2018-04-24 18:27:36 +0800

    save code

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # 切换node版本
-# source /etc/profile && nvm use 8 &&  echo 'node version is' && node -v
+source /etc/profile && nvm use 8 &&  echo 'node version is' && node -v
 
 # 删除node_modules软链接
 rm -rf node_modules
 
-node ./src/_compile.js
+cd ./src
+
+# 安装依赖
+npm install --registry=http://npm.byted.org
+
+node ./_compile.js
+
+cd ../
 
 #将代码移出
 cp -r ./dist/* ./
@@ -14,9 +21,6 @@ cp -r ./dist/* ./
 rm -rf dist
 
 rm -rf src
-
-# 安装依赖
-npm install --registry=http://npm.byted.org
 
 # 创建输出目录
 DIR=`pwd`
